0cc4cfc0a730971bc4464d7fad8766a405e9c248
chsong <bsk9212@gmail.com>
2023-03-22 12:05:26 +0900

    feat(posts): fs 이용하여 md파일 불러오기

--- app/blog/page.tsx
+++
@@ -1,4 +0,0 @@
-const BlogPage = () => {
-  return <h1> HI Blog</h1>;
-};
-export default BlogPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+import fs from "fs";
+
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
 const HomePage = () => {
-  return <h1> HI </h1>;
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ));
+  return <div>{postPreviews}</div>;
 };
 export default HomePage;
